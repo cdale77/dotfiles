b3caf5b2e85cd9dd80418c3183f2af07822ea166
Brad Johnson <brad_j@beezwax.net>
2019-01-29 20:55:38 -0800

    add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+set -euo pipefail
+
+echo "Symlinking dotfiles . . ."
+
+script_path="$( cd "$(dirname "$0")" ; pwd -P )"
+
+ln -sfv $script_path/.gitignore_global ~/.gitignore_global
+ln -sfv $script_path/.rspec ~/.rspec
+ln -sfv $script_path/.tern-config ~/.tern-config
+ln -sfv $script_path/.tmux.conf ~/.tmux.conf
+ln -sfv $script_path/.zshrc ~/.zshrc
+ln -sfv $script_path/init.vim ~/.config/nvim/
